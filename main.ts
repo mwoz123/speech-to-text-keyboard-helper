cb91274018c101432a42e603a84fbeb84dd18cda
mwoz123 <4487025+mwoz123@users.noreply.github.com>
2023-08-16 21:17:17 +0200

    refator: generate common fn() + remove unnecessary id prefixes

--- main.ts
+++ main.ts
@@ -1,29 +1,32 @@
 import { Editor, Plugin } from 'obsidian';
 
 export default class SpeechToTextKeyboardHelper extends Plugin {
-	commands = [
-      {
-        name: "Prepend new line (and jump to it)",
-        id: "speach2text-kb-prepend-new-line",
-        editorCallback: (editor: Editor) => {
-          const line = editor.getCursor().line;
-          editor.replaceRange("\n", { line, ch: 0 });
-          editor.setCursor({ line, ch: 0 });
-        }
-      },
-      {
-        name: "Append new line (and jump to it)",
-        id: "speach2text-kb-append-new-line",
-        editorCallback: (editor: Editor) => {
-          const line = editor.getCursor().line + 1;
-          editor.replaceRange("\n", { line, ch: 0 });
-          editor.setCursor({ line, ch: 0 });
-        }
-      },
-	]
+  commands = [
+    {
+      name: "Prepend new line (and jump to it)",
+      id: "prepend-new-line",
+      editorCallback: (editor: Editor) => {
+        const line = editor.getCursor().line;
+        createNewLineBeforeGivenLineAndSetCursorPossitionToIt(editor, line);
+      }
+    },
+    {
+      name: "Append new line (and jump to it)",
+      id: "append-new-line",
+      editorCallback: (editor: Editor) => {
+        const line = editor.getCursor().line + 1;
+        createNewLineBeforeGivenLineAndSetCursorPossitionToIt(editor, line);
+      }
+    },
+  ]
 
-	async onload() {
-		this.commands.map(e => this.addCommand(e));
-	}
+  async onload() {
+    this.commands.map(e => this.addCommand(e));
+  }
+}
+
+function createNewLineBeforeGivenLineAndSetCursorPossitionToIt(editor: Editor, line: number) {
+  editor.replaceRange("\n", { line, ch: 0 });
+  editor.setCursor({ line, ch: 0 });
 }
 
